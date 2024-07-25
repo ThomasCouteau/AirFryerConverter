be4a6b8b366cb80cd36ca00f1feb760f6521ec8c
couteaut <couteaut@3520-JNN4V93>
2024-07-25 13:23:29 +0200

    📦 NEW: airfryer converter

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,131 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed, watchEffect } from 'vue';
+import IconTimer from '@/components/icons/IconTimer.vue';
+import IconThermometer from '@/components/icons/IconThermometer.vue';
+
+const tempFurnace = ref(null);
+const timeFurnace = ref(null);
+const tempAirFryer = ref(null);
+const timeAirFryer = ref(null);
+
+const convertToAirfryer = () => {
+  const tempFour = Number(tempFurnace.value);
+  const timeFour = Number(timeFurnace.value);
+
+  if (isNaN(tempFour) || isNaN(timeFour)) {
+    tempAirFryer.value = 'Erreur';
+    timeAirFryer.value = 'Erreur';
+    return;
+  }
+
+  let tempAirfryer, timeAirfryer;
+
+  if (tempFour > 200) {
+    tempAirfryer = 200;
+    const adjustedTime = timeFour * ((tempFour - 200) / (220 - 200) + 1);
+    timeAirfryer = adjustedTime * 0.8;
+  } else {
+    tempAirfryer = tempFour;
+    timeAirfryer = timeFour * 0.8;
+  }
+
+  timeAirfryer = Math.round(timeAirfryer);
+
+  tempAirFryer.value = tempAirfryer.toFixed(0);
+  timeAirFryer.value = timeAirfryer;
+};
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1 class="text-2xl">AirFryer Converter</h1>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="flex items-center justify-center">
+    <div class="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img src="@/assets/airfryer.jpg" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Convertisseur pour Air Fryer!</h2>
+        <form
+          @submit.prevent="convertToAirfryer"
+          method="post"
+          class="flex flex-col space-y-4"
+        >
+          <div class="flex flex-col items-center space-y-2">
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text"
+                  >Quelle est la température du four ?</span
+                >
+              </div>
+              <label class="input input-bordered flex items-center gap-2">
+                <IconThermometer />
+                <input
+                  v-model="tempFurnace"
+                  type="text"
+                  inputmode="tel"
+                  class="grow"
+                  placeholder="Température"
+                />
+                °C
+              </label>
+            </label>
+
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text"
+                  >Quel est le temps à mettre au four ?</span
+                >
+              </div>
+              <label class="input input-bordered flex items-center gap-2">
+                <IconTimer />
+                <input
+                  v-model="timeFurnace"
+                  type="text"
+                  inputmode="tel"
+                  class="grow"
+                  placeholder="Temps"
+                />
+                Min
+              </label>
+            </label>
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text">Température au AirFryer:</span>
+              </div>
+              <div class="input input-bordered flex items-center gap-2">
+                <IconThermometer />
+                <input
+                  v-model="tempAirFryer"
+                  type="text"
+                  class="grow"
+                  readonly
+                />
+                °C
+              </div>
+            </label>
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text">Temps au AirFryer:</span>
+              </div>
+              <div class="input input-bordered flex items-center gap-2">
+                <IconTimer /><input
+                  v-model="timeAirFryer"
+                  type="text"
+                  class="grow"
+                  readonly
+                />
+                Min
+              </div>
+            </label>
+          </div>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" type="submit">Convertir</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
